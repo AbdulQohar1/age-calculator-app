6495b34efe227dbd0a30f77fa0a21a1cc6f2054a
AbdulQohar1 <adebayoabdulqohar1@gmail.com>
2023-10-08 12:29:32 +0100

    age func implementation and refactoring

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const birthDay = document.querySelector('input[name=days]').value;
-const birthMonth = document.querySelector('input[name=months]').value;
-const birthYear = document.querySelector('input[name=years]').value;
+let birthDay
+let birthMonth
+let birthYear
 const submitBtn = document.querySelector('.svg-arrow');
-const requiredMessage = document.querySelectorAll('.required-msg');
-
+const requiredMessage1 = document.getElementById('required-msg-1');
+const requiredMessage2 = document.getElementById('required-msg-2');
+const requiredMessage3 = document.getElementById('required-msg-3');
+// requiredMessage.innerHTML = "This field is required"; 
 let userAge = document.querySelector('.year-output');
 let userMonth = document.querySelector('.months-output');
 let userDays = document.querySelector('.days-output');
@@ -12,40 +14,92 @@ let date = new Date();
 // new Date() is a javascript method that collects time from your device
 let currentDay = date.getDate();
 let currentMonth = 1 + date.getMonth();
+console.log(currentMonth);
 let currentYear = date.getFullYear();
-let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; 
 
-const ageFunction = function() {
-  if( birthDay ==='' || birthMonth ==='' || birthYear ==='' ) {
-    requiredMessage.innerHTML = "This field is required"; 
-    console.log("this field is required");
+const lastDayOfCurrentMonth = (new Date(currentYear, currentMonth, 0 )).getDate();
+
+const checkEmptyStateFunction = function(){
+  if (birthDay =='') {
+    requiredMessage1.innerHTML = `This field is required`; 
+    return
+  } 
+  else if (birthYear =='') {
+    requiredMessage3.innerHTML = `This field is required`; 
+    return
   }
-  else{
+  else if (birthMonth =='') {
+    requiredMessage2.innerHTML = `This field is required`;
+    return
+  }
+  else if (birthMonth.value > 12) {
+    requiredMessage2.innerHTML = `Must be a valid Month`;
+    return
+  } 
+  calendarSimulationFunction();
+}
 
+
+
+
+
+let calendarSimulationFunction = function() {
+  if (birthYear > currentYear ) {
+    requiredMessage3.innerHTML = `Must be in the past`;
+    return;
+  } 
+  else if (birthMonth > 12) {
+    requiredMessage2.innerHTML = `Must be a valid Month`;
+    return;
+  }
+  else if (birthDay > lastDayOfCurrentMonth) {
+    requiredMessage1.innerHTML = `Must be a valid day`;
+    console.log('Must be a valid day');
+    return;
+  };
+  calcAgeFunction();
+
+}
+
+const calcAgeFunction = function() {
   if (birthDay > currentDay) {
     currentDay = currentDay + month[currentMonth - 1];
     currentMonth = currentMonth - 1; 
-
   }
   if (birthMonth > currentMonth) {
     currentMonth = currentMonth + 12;
     currentYear = currentYear - 1;
   }
-   userDays.innerHTML = currentDay - birthDay;
-   userMonth.innerHTML = currentMonth - birthMonth;
-   userAge.innerHTML = currentYear - birthYear;
-
-
+  userDays.innerHTML = currentDay - birthDay;
+  userMonth.innerHTML = currentMonth - birthMonth;
+  userAge.innerHTML = currentYear - birthYear;
   console.log(userAge);
   console.log(userMonth);
   console.log(userDays);
 }
 
+
+
+
+let label = document.querySelectorAll('label');
+
+const ageFunction = function() {
+  birthDay = document.querySelector('input[name=days]').value;
+  birthMonth = document.querySelector('input[name=months]').value;
+  birthYear = document.querySelector('input[name=years]').value;
+
+
+  checkEmptyStateFunction();
+  // calendarSimulationFunction();
+  // calcAgeFunction();
 }
-ageFunction()
 
 submitBtn.addEventListener( "click", ageFunction);
 
 
+
+
+  
 
 
